3d73e79f2b9f717be89c37b3f0c532982c594066
JAYANTH-devolper <jayanths507@gmail.com>
2025-03-29 17:31:02 +0530

    USER INPUT(SCANNER CLASS)

---
+++ demo2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+//A program that related to ask a user name and greeting them//
+//use built in functions are :-data types ,input class//
+public class demo2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter your name please");
+        String name=sc.nextLine();
+        System.out.println("you are most welcome to the world of code buddy ,have great code time ");
+        
+        
+    }
+    
+}
