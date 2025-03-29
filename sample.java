a9b7395201e41a723bd046fee0eb93a7a2d33d4a
JAYANTH-devolper <jayanths507@gmail.com>
2025-03-29 14:31:59 +0530

    sum of two number

--- sample.java
+++ sample.java
@@ -1,6 +1,10 @@
-class sample{
-    public static void main(String[] args){
-        // just statment print program//
-System.out.println("hello world");
-    }
+ class sample {
+    
+    public static void main (String[] args){
+        // this program is related about su of two numbers // 
+        int a=10;
+        int b=30;
+        int c= a+b;
+        System.out.println(c);
+    } 
 }
