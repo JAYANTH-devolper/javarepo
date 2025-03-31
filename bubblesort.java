c5466401df68169d673dae66327416b0e12db52b
JAYANTH-devolper <jayanths507@gmail.com>
2025-03-31 19:48:19 +0530

    SORTING(BUBBLE SORT IN DECENDING ORDER)

--- bubblesort.java
+++ bubblesort.java
@@ -7,7 +7,7 @@ public class bubblesort {
  for(int i=0;i<=n-1;i++){
     int flag=0;
     for(int j=0;j<=n-1-i;j++){
-        if(arr[j]>arr[j+1]){
+        if(arr[j]<arr[j+1]){
             int temp=arr[j];
             arr[j]=arr[j+1];
             arr[j+1]=temp;
